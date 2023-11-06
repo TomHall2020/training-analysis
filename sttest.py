0c7f6bf945c0bf6f3e6aadeba5c72494d55dcd16
Tom Hall <thall-tom@hotmail.co.uk>
2023-11-06 21:36:23 +0000

    Refactor controls and put file upload within form

--- sttest.py
+++ sttest.py
@@ -7,16 +7,18 @@ from analysis.data import ewm_plot
 
 
 @st.cache_data
-def process_csv(csv):
+def process_csv(csv, dateformat="ISO"):
     df = pd.read_csv(csv, header=0, names=["date", "volume"])
-    df["date"] = pd.to_datetime(df["date"])
+
+    kw = dateformats[dateformat][1]
+    df["date"] = pd.to_datetime(df["date"], **kw)
     return df.astype({"volume": DType.U16})
 
 
-def filter_and_plot_data():
+def filter_and_plot_data(start, end):
     # provided dates from widget are datetime.date instances
     # pandas only plays ball with datetime.datetime instances or strings
-    mask = (df.date >= start_date.isoformat()) & (df.date <= end_date.isoformat())
+    mask = (df.date >= start.isoformat()) & (df.date <= end.isoformat())
     selected = df.loc[mask]
 
     chart = ewm_plot(selected, windows=(10, 30, 90))
@@ -25,58 +27,84 @@ def filter_and_plot_data():
     return chart
 
 
-def reset_start_date():
-    st.session_state.start_date = df.date.min()
-
-
-def reset_end_date():
-    st.session_state.end_date = df.date.max()
+def reset_date(name, value):
+    st.session_state[name] = value
 
 
 # App
-
 st.set_page_config(
     page_title="Volume Analysis",
     page_icon="📊",
+    layout="wide",
 )
 st.title("Toms Volume analyser")
-st.markdown(
+help = st.container()
+data_sidebar = st.sidebar.container()
+data_info = st.expander("Data statistics")
+date_selectors = st.container()
+chart_display = st.container()
+
+with help:
+    st.markdown(
+        """
+    To use, simply upload a csv file of your volumes with two columns, date and volume.
+    eg:
+    ```csv
+    date,volume
+    20/10/2016,200
+    21/10/2016,100
+    ```
     """
-To use, simply upload a csv file of your volumes with two columns, date and volume.
-eg:
-```csv
-date,volume
-20/10/2016,200
-21/10/2016,100
-```
-"""
-)
-csv = st.file_uploader("Upload volume csv file here", type=".csv")
+    )
+
+dateformats = {
+    "ISO": ("YYYY-MM-DD", dict(format="ISO8601")),
+    "Day first": ("DD/MM/YYYY", dict(dayfirst=True)),
+    "Month first": ("MM/DD/YYYY", dict(dayfirst=False)),
+}
+with data_sidebar:
+    st.header("Data Input")
+    with st.form(key="data-input"):
+        csv = st.file_uploader("Upload volume csv file here", type=".csv", key="csv")
+        datefmt = st.selectbox(
+            "Choose a date format",
+            [*dateformats.keys()],
+            format_func=lambda option: f"{option} ({dateformats[option][0]})",
+            index=1,
+            key="datefmt",
+        )
+        st.form_submit_button("Analyse")
 if csv:
-    df = process_csv(csv)
+    st.session_state["df"] = df = process_csv(csv, st.session_state.datefmt)
 else:
-    df = process_csv("data/Training_Vol18-19.csv")
+    st.session_state["df"] = df = process_csv("data/Training_Vol18-19.csv", datefmt)
 
-st.session_state["df"] = df
+# with st.expander("Data statistics"):
+with data_info:
+    left, right = st.columns(2)
+    left.dataframe(df["volume"].describe())
+    right.dataframe(df.head())
 
-with st.expander("Data statistics"):
-    st.dataframe(df["volume"].describe())
-    st.write(df.dtypes)
 
 # date filters
-with st.container():
+with date_selectors:
     left, right = st.columns(2)
     selected = df.copy()
     start_date = left.date_input("Start Date", df.date.min(), key="start_date")
-    left.button("Reset", key="reset_start", on_click=reset_start_date)
+    left.button(
+        "Reset",
+        key="reset_start",
+        on_click=reset_date,
+        args=["start_date", df.date.min()],
+    )
     end_date = right.date_input("End Date", df.date.max(), key="end_date")
-    right.button("Reset", key="reset_end", on_click=reset_end_date)
+    right.button(
+        "Reset",
+        key="reset_end",
+        on_click=reset_date,
+        args=["end_date", df.date.max()],
+    )
 
-chart = filter_and_plot_data()
-st.altair_chart(chart)
-# experimental section
-# st.markdown("---")
-
-# extra = st.data_editor(df.tail())
-
-# st.altair_chart(ewm_plot(extra))
+with chart_display:
+    chart = filter_and_plot_data(start_date, end_date)
+    st.altair_chart(chart)
